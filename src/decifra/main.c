f171018b48fc99b1b448c24c3d39079f1179608a
Geovane Saraiva da Silva <70657835+0LostConnection@users.noreply.github.com>
2024-09-14 17:19:01 -0300

    Fix: includes concertados

--- src/decifra/main.c
+++ src/decifra/main.c
@@ -1,8 +1,5 @@
 #include "utils.h"
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
 
 int main() {
     const char *hex_string = "566F6388732073C66F2076656E6365646F867265732C20766F63C3887320636FBE6E73656775656D2E002DC6C921B7B87FCF";
